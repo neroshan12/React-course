09169a160334d8643fcdb9e94d9bcf455ae9814c
Nero Siva <neroshan12@gmail.com>
2018-03-12 14:42:36 +0000

    Add first React component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
+
+/*
+js library that is used to produce html that is shown to a user in a browser
+writing individual components or views. Components are snippets of codes that produces HTML
+
+*/
+
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import ReactDOM from  'react-dom';
 
-import App from './components/app';
-import reducers from './reducers';
+// 1. Create new component
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => { //const = final value and wont change and won't reassign app further down the line //app is a type of component, it is a class // need to instantiate component before passing it to the DOM
+    return <div>Hi!</div>; // it is a component instance in JSX
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+
+
+
+
+
+// 2. some HTML
+// 3. Take this component's generated HTML and put it on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container')); // trying to update the dom not react // (<App />) creates an instance of App
